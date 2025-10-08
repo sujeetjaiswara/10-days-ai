7b510526ee69f6450f0b8edcc3848b7b4658d56c
Sujeet Jaiswara <sujeet@cleartrust.cc>
2025-10-08 11:31:17 +0530

    Code refector

--- livekit-learn/livekit-demo1/frontend/src/App.tsx
+++ livekit-learn/livekit-demo1/frontend/src/App.tsx
@@ -1,21 +1,15 @@
 import {
   ControlBar,
-  GridLayout,
-  ParticipantTile,
   RoomAudioRenderer,
-  useTracks,
   RoomContext,
 } from "@livekit/components-react";
-import { Room, Track } from "livekit-client";
 import "@livekit/components-styles";
+import { Room } from "livekit-client";
 import { useEffect, useState } from "react";
+import MyVideoConference from "./components/MyVideoConference";
 
-const serverUrl = "wss://myfirstproj-bm15e77z.livekit.cloud";
-
-// TODO: Replace with a token generated from your backend
-// Get this token after login from your backend
-const token =
-  "eyJhbGciOiJIUzI1NiJ9.eyJ2aWRlbyI6eyJyb29tSm9pbiI6dHJ1ZSwicm9vbSI6IlJvb20xIn0sImlzcyI6IkFQSUdWbkpNRm16M0ZGUSIsImV4cCI6MTc1OTkyMzUzNywibmJmIjowLCJzdWIiOiJTdWplZXQifQ.KA6dRef5aZaukfxWO2blmUa18ILv81nopzBeSuFoMJI";
+const serverUrl = import.meta.env.VITE_LIVEKIT_URL;
+const token = import.meta.env.VITE_LIVEKIT_TOKEN;
 
 function App() {
   const [room] = useState(
@@ -23,6 +17,7 @@ function App() {
       new Room({
         // Optimize video quality for each participant's screen
         adaptiveStream: true,
+
         // Enable automatic audio/video quality optimization
         dynacast: true,
       })
@@ -50,35 +45,14 @@ function App() {
       <div data-lk-theme="default" style={{ height: "70vh" }}>
         {/* Your custom component with basic video conferencing functionality. */}
         <MyVideoConference />
+
         {/* The RoomAudioRenderer takes care of room-wide audio for you. */}
         <RoomAudioRenderer />
+
         {/* Controls for the user to start/stop audio, video, and screen share tracks */}
         <ControlBar />
       </div>
     </RoomContext.Provider>
-  );
-}
-
-function MyVideoConference() {
-  // `useTracks` returns all camera and screen share tracks. If a user
-  // joins without a published camera track, a placeholder track is returned.
-  const tracks = useTracks(
-    [
-      { source: Track.Source.Camera, withPlaceholder: true },
-      { source: Track.Source.ScreenShare, withPlaceholder: false },
-    ],
-    { onlySubscribed: false }
-  );
-
-  return (
-    <GridLayout
-      tracks={tracks}
-      style={{ height: "calc(100vh - var(--lk-control-bar-height))" }}
-    >
-      {/* The GridLayout accepts zero or one child. The child is used
-      as a template to render all passed in tracks. */}
-      <ParticipantTile />
-    </GridLayout>
   );
 }
 
