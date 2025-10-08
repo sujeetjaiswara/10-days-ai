c6137009598732481d10057c1fb285214b174de3
Sujeet Jaiswara <sujeet@cleartrust.cc>
2025-10-08 11:20:30 +0530

    Added - git ignore file

--- livekit-learn/livekit-demo1/frontend/src/App.tsx
+++ livekit-learn/livekit-demo1/frontend/src/App.tsx
@@ -1,35 +1,85 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {
+  ControlBar,
+  GridLayout,
+  ParticipantTile,
+  RoomAudioRenderer,
+  useTracks,
+  RoomContext,
+} from "@livekit/components-react";
+import { Room, Track } from "livekit-client";
+import "@livekit/components-styles";
+import { useEffect, useState } from "react";
+
+const serverUrl = "wss://myfirstproj-bm15e77z.livekit.cloud";
+
+// TODO: Replace with a token generated from your backend
+// Get this token after login from your backend
+const token =
+  "eyJhbGciOiJIUzI1NiJ9.eyJ2aWRlbyI6eyJyb29tSm9pbiI6dHJ1ZSwicm9vbSI6IlJvb20xIn0sImlzcyI6IkFQSUdWbkpNRm16M0ZGUSIsImV4cCI6MTc1OTkyMzUzNywibmJmIjowLCJzdWIiOiJTdWplZXQifQ.KA6dRef5aZaukfxWO2blmUa18ILv81nopzBeSuFoMJI";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [room] = useState(
+    () =>
+      new Room({
+        // Optimize video quality for each participant's screen
+        adaptiveStream: true,
+        // Enable automatic audio/video quality optimization
+        dynacast: true,
+      })
+  );
+
+  // Connect to room
+  useEffect(() => {
+    let mounted = true;
+
+    const connect = async () => {
+      if (mounted) {
+        await room.connect(serverUrl, token);
+      }
+    };
+    connect();
+
+    return () => {
+      mounted = false;
+      room.disconnect();
+    };
+  }, [room]);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <RoomContext.Provider value={room}>
+      <div data-lk-theme="default" style={{ height: "70vh" }}>
+        {/* Your custom component with basic video conferencing functionality. */}
+        <MyVideoConference />
+        {/* The RoomAudioRenderer takes care of room-wide audio for you. */}
+        <RoomAudioRenderer />
+        {/* Controls for the user to start/stop audio, video, and screen share tracks */}
+        <ControlBar />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </RoomContext.Provider>
+  );
 }
 
-export default App
+function MyVideoConference() {
+  // `useTracks` returns all camera and screen share tracks. If a user
+  // joins without a published camera track, a placeholder track is returned.
+  const tracks = useTracks(
+    [
+      { source: Track.Source.Camera, withPlaceholder: true },
+      { source: Track.Source.ScreenShare, withPlaceholder: false },
+    ],
+    { onlySubscribed: false }
+  );
+
+  return (
+    <GridLayout
+      tracks={tracks}
+      style={{ height: "calc(100vh - var(--lk-control-bar-height))" }}
+    >
+      {/* The GridLayout accepts zero or one child. The child is used
+      as a template to render all passed in tracks. */}
+      <ParticipantTile />
+    </GridLayout>
+  );
+}
+
+export default App;
